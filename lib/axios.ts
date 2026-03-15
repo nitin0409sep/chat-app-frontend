0e7fb0f52738661d44a8454f2b1c38c15c450ccc
Nitin Verma <nitin.verma@remotestate.in>
2026-03-15 17:01:15 +0530

    Updated CI CD

--- lib/axios.ts
+++ lib/axios.ts
@@ -6,9 +6,6 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
   ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1`
   : "";
 
-
-console.log({ API_BASE_URL });
-
 // ─── 'Axios Instances ────────────────────────────────────────────────
 export const publicAxios = axios.create({
   baseURL: API_BASE_URL,
